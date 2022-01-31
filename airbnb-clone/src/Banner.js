cfe75ce97721c5cfdf916a371d7fd865edda92ec
Shahed Ahmad <shaheda.a16@gmail.com>
2022-01-31 22:15:09 +0430

    banner and Search with guest and date picker added

--- airbnb-clone/src/Banner.js
+++ airbnb-clone/src/Banner.js
@@ -1,12 +1,25 @@
-import { Button } from '@material-ui/core';
-import React from 'react';
+import React, { useState } from 'react'
 import './Banner.css'
+import { Button } from "@material-ui/core";
+import Search from './Search';
 
 const Banner = () => {
+    const [showSearch, setShowSearch] = useState(false);
   return <div className='banner'>
-      <h1>I</h1>
-      <Button>My Button</Button>
-  </div>;
+  <div className='banner__search'>
+      {showSearch && <Search />}
+
+      <Button onClick={() => setShowSearch(!showSearch)} className='banner__searchButton' variant='outlined'>
+          {showSearch ? "Hide" : "Search Dates"}
+      </Button>
+  </div>
+  <div className='banner__info'>
+      <h1>Get out and stretch your imagination</h1>
+      <h5>
+          Plan a different kind of getaway to uncover the hidden gems near you.
+      </h5>
+  </div>
+</div>
 };
 
 export default Banner;
