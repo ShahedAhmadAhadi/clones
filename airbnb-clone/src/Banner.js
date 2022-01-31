9ce4c5258915db5e1a3f47971a886f87feca2a6f
Shahed Ahmad <shaheda.a16@gmail.com>
2022-01-31 21:48:33 +0430

    Banner component with styles for home

---
+++ airbnb-clone/src/Banner.js
@@ -0,0 +1,12 @@
+import { Button } from '@material-ui/core';
+import React from 'react';
+import './Banner.css'
+
+const Banner = () => {
+  return <div className='banner'>
+      <h1>I</h1>
+      <Button>My Button</Button>
+  </div>;
+};
+
+export default Banner;
