1a5e43c2d7f1068ca64fc6497fab4e936be09fec
Shahed Ahmad <shaheda.a16@gmail.com>
2022-02-09 22:59:26 +0430

    basic styling and structure

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -3,7 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-      
+      <div className="app__header">
+                {<img 
+                className="app__headerImage"
+                height="40px;"
+                src="https://toogreen.ca/instagreen/img/instagreen.svg"
+                alt="insta logo"
+              /> }
+            </div>
     </div>
   );
 }
