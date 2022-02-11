74acb4ca5ae5a7fe9d068c6043c161132c9e292e
Shahed Ahmad <shaheda.a16@gmail.com>
2022-02-11 22:08:09 +0430

    firebase setup configured and got data from firebase

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
-import Post from './Post';
+import React, {useState, useEffect} from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="app">
-      <div className="app__header">
+import Post from './Post'
+import {db} from './firebase'
+
+const App = () => {
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        db.collection('posts').onSnapshot(snapshot => {
+            setPosts(snapshot.docs.map(doc => doc.data()))
+        })
+    })
+
+    return (
+        <div className='app'>
+            <div className="app__header">
                 {<img 
                 className="app__headerImage"
                 height="40px;"
@@ -13,13 +24,17 @@ function App() {
               /> }
             </div>
 
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-    </div>
-  );
+            {
+                posts.map(post => (
+                    <Post
+                        username={post.username}
+                        caption={post.caption}
+                        imageUrl={post.imageUrl}
+                    />
+                ))
+            }
+        </div>
+    )
 }
 
-export default App;
+export default App
