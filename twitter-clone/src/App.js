128203c9ecc982f04d2b8b2c6b673fad8c9cb15d
Shahed Ahmad <shaheda.a16@gmail.com>
2022-02-21 13:28:50 +0430

    sidebar added and added to app

--- twitter-clone/src/App.js
+++ twitter-clone/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import Feed from './Feed';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="App">
+      <Sidebar />
       <Feed />
     </div>
   );
