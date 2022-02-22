2be1333d6b08550d2409fc53bf406d2a08bb2684
Shahed Ahmad <shaheda.a16@gmail.com>
2022-02-22 15:12:36 +0430

    twitter errors removed

--- twitter-clone/src/App.js
+++ twitter-clone/src/App.js
@@ -1,17 +1,17 @@
-import React from "react";
-import Sidebar from "./Sidebar";
-import Feed from "./Feed";
-import Widgets from "./Widgets";
-import "./App.css";
+import React from 'react';
+import Sidebar from './Sidebar';
+import Feed from './Feed';
+import Widgets from './Widgets';
+import './App.css';
 
 function App() {
-  return (
-    <div className="app">
-      <Sidebar />
-      <Feed />
-      <Widgets />
-    </div>
-  );
+    return (
+        <div className="app">
+            <Sidebar />
+            <Feed />
+            <Widgets />
+        </div>
+    );
 }
 
 export default App;
