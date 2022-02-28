69c2042dd61dcdfb0f84e40aaf20db5ff828533e
Shahed Ahmad <shaheda.a16@gmail.com>
2022-02-28 21:12:14 +0430

    logo added

--- tesla-clone/src/components/Header.js
+++ tesla-clone/src/components/Header.js
@@ -1,11 +1,32 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Header = () => {
     return (
         <div>
-            Hello from Header components
+            <Container >
+                <a>
+                    <img src="/images/logo.svg" alt="" />
+                </a>
+                <Menu>
+
+                </Menu>
+            </Container>
         </div>
     )
 }
 
 export default Header
+
+const Container = styled.div`
+    min-height: 60px;
+    position: fixed;
+    display: flex;
+    align-items: center;
+    padding: 0 20px;
+    
+`
+
+const Menu = styled.div`
+
+`
