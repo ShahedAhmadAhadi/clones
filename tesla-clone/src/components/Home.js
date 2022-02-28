bd4cea387d47225656306764b2b311591ceba981
Shahed Ahmad <shaheda.a16@gmail.com>
2022-02-28 21:08:50 +0430

    Props drilling removed by configuring with redux

--- tesla-clone/src/components/Home.js
+++ tesla-clone/src/components/Home.js
@@ -6,7 +6,34 @@ const Home = () => {
     return (
         <div>
             <Container>
-                <Section />
+                <Section
+                    title='Model S'
+                    description='Order Online For Touchless Delivery'
+                    backgroundImage='model-s.jpg'
+                    leftBtnText='Custom Order'
+                    rightBtnText='Existing Inventory'
+                />
+                <Section
+                    title='Model Y'
+                    description='Order Online For Touchless Delivery'
+                    backgroundImage='model-y.jpg'
+                    leftBtnText='Custom Order'
+                    rightBtnText='Existing Inventory'
+                />
+                <Section
+                    title='Model 3'
+                    description='Order Online For Touchless Delivery'
+                    backgroundImage='model-3.jpg'
+                    leftBtnText='Custom Order'
+                    rightBtnText='Existing Inventory'
+                />
+                <Section
+                    title='Model X'
+                    description='Order Online For Touchless Delivery'
+                    backgroundImage='model-x.jpg'
+                    leftBtnText='Custom Order'
+                    rightBtnText='Existing Inventory'
+                />
             </Container>
         </div>
     )
--- tesla-clone/src/components/Section.js
+++ tesla-clone/src/components/Section.js
@@ -1,21 +1,21 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Section = () => {
+const Section = ({ title, description, backgroundImage, leftBtnText, rightBtnText }) => {
     return (
-        <Wrap>
+        <Wrap bgImage={backgroundImage}>
             <ItemText>
-                <h1>Model S</h1>
-                <p>Order Online For Touchless Delivery</p>
+                <h1>{title}</h1>
+                <p>{description}</p>
             </ItemText>
 
             <Buttons>
                 <ButtonGroup>
                     <LeftButton>
-                        Custom Order
+                        {leftBtnText}
                     </LeftButton>
                     <RightButton>
-                        Existing Inventory
+                        {rightBtnText}
                     </RightButton>
                 </ButtonGroup>
                 <DownArrow src='/images/down-arrow.svg.png' />
@@ -29,7 +29,7 @@ export default Section
 const Wrap = styled.div`
     width: 100vw;
     height: 100vh;
-    background-image: url('/images/model-s.jpg');
+    background-image:${props => `url("/images/${props.bgImage}")`};
     background-size: cover;
     background-position: center ;
     background-repeat: no-repeat;
@@ -47,6 +47,9 @@ const ItemText = styled.div`
 const ButtonGroup = styled.div`
     display :flex;
     margin-bottom: 30px;
+    @media (max-width: 768px){
+        flex-direction : column;
+    }
     `
 
 const LeftButton = styled.div`
@@ -63,10 +66,13 @@ const LeftButton = styled.div`
     font-size : 12px;
     cursor: pointer;
     margin: 8px;
-`
+    `
 
 const RightButton = styled(LeftButton)`
-
+    background-color: white;
+    opacity: .65;
+    color: black;
+    font-weight: 550;
 `
 
 const DownArrow = styled.img`
